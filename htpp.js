0c225113eb6f6a9c42ddc1e224f6555a496928a1
heyangjiedd <1059320200@qq.com>
2018-06-07 19:19:18 +0800

    测试一下

--- htpp.js
+++ htpp.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (request,response) {
    response.writeHead(200,{'Content-Type':'text/plain'});
-    response.end('Hello World12313\n');
+    response.end('Hello World123133434\n');
 }).listen(8888);
 console.log('start already')
